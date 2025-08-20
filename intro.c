45325f3882e3aa604c087d41aa7c5400303e503b
s4sahiko <s4sahiko@gmail.com>
2025-08-20 22:21:11 +0530

    Create intro.c

---
+++ intro.c
@@ -0,0 +1,12 @@
+/*
+C-Tutorial
+This file is part of the C-Tutorial project.
+Author: Sahiko
+*/
+#include <stdio.h> //preprocessor directive 
+int main() //main function
+{
+    printf("Hello, World!\n"); //output statement
+    return 0; //return statement //indicates successful execution
+} //end of main function
+

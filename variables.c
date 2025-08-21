09a3a5522a9e0f6520db7ae8e1aadb156d13966d
s4sahiko <s4sahiko@gmail.com>
2025-08-21 14:49:43 +0530

    Create variables.c

---
+++ variables.c
@@ -0,0 +1,16 @@
+/*
+Variables in C
+Variables are used to store data in a program. They must be declared before they can be used.
+In C, a variable must be declared with a specific data type, which determines the kind of
+we will further be studying data types.
+*/
+
+#include <stdio.h>
+int main() {
+    int a; // Declaration of variable 'a'
+    a = 10; // Initialization of variable 'a'
+    printf("The Value of a is %d\n", a);
+    return 0;
+}
+
+

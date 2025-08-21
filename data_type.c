0d5502f648f3e7e71d8bdfbc985ed63e9fd82e95
s4sahiko <s4sahiko@gmail.com>
2025-08-21 14:50:06 +0530

    Create data_type.c

---
+++ data_type.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int a;  // Declaration of variable 'a'
+    a = 10; // Initialization of variable 'a'
+    printf("The Value of a is %d\n", a);
+// Further variable declarations
+    float b; // Declaration of variable 'b'
+    b = 5.5; // Initialization of variable 'b'
+    printf("The Value of b is %f\n", b);
+// Further variable declarations
+    char c; // Declaration of variable 'c'
+    c = 'A'; // Initialization of variable 'c'
+    printf("The Value of c is %c\n", c);
+// Further variable declarations
+    double d; // Declaration of variable 'd'
+    d = 3.14159; // Initialization of variable 'd'
+    printf("The Value of d is %lf\n", d);
+    return 0;   
+}
